4b31bdc4857d4548916007cacd5600ae10dd9034
alexmarkova <odm215@nyu.edu>
2018-05-10 13:44:05 +0400

    yo

--- scripts.js
+++ scripts.js
@@ -1,35 +1,43 @@
+// HOME BUTTON
 var homeButton = document.getElementById("home");
-
-var doSomethingHome = function(){
-	document.location.href = "index.html"; 
-	console.log("go back to home page");
-};
-
+var doSomethingHome = function() {
+document.location.href = "https://alexmarkova.github.io";
+}
 homeButton.addEventListener("click", doSomethingHome);
 
-//select for the button
-var animationButton = document.getElementById("animation");
-
-//definei a function to call
-var doSomethingAnimation = function(){
-	document.location.href = "https://russell-c.github.io/memories/index.html"; 
+//	SOUND BUTTON
+var soundButton = document.getElementById("sound");
+var doSomethingSound = function () {
+document.location.href = "https://manaspant.github.io/CommLabAudio/";
 }
+soundButton.addEventListener("click", doSomethingSound);
 
-//when the button is clicked, do the function
+//VIDEO BUTTON
+var videoButton = document.getElementById("video");
+var doSomethingVideo = function () {
+document.location.href = "https://ambernomani.github.io/how-to-halloween/";
+}
+videoButton.addEventListener("click", doSomethingVideo);
+
+// ANIMATION BUTTON
+var animationButton = document.getElementById("animation");
+var doSomethingAnimation = function() {
+document.location.href = "https://russell-c.github.io/memories/index.html"; 
+}
 animationButton.addEventListener("click", doSomethingAnimation);
 
+// COMIC BUTTON
+var comicButton = document.getElementById("comic");
+var doSomethingComic = function () {
+document.location.href = "https://russell-c.github.io/theLittleThings/welcome.html";
+}
+comicButton.addEventListener("click", doSomethingComic);
 
-//MOVIE BUTTON
+// MOVIE BUTTON
 var movieButton = document.getElementById("movie");
 var doSomethingMovie = function () {
-	document.location.href = "https://alexmarkova.github.io/movie/";
+document.location.href = "https://alexmarkova.github.io/movie/";
 }
 movieButton.addEventListener("click", doSomethingMovie);
 
-// SOUND BUTTON
 
-// COMIC
-
-// VIDEO
-
-// ANIMATION
